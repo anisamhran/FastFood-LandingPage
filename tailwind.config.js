eb5e6546ed5abf4619e1b0cbf854dd87954a3ec7
anisamhran <117977576+anisamhran@users.noreply.github.com>
2024-03-27 21:17:14 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./src/**/*.{html,js}",
+      'node_modules/preline/dist/*.js',
+      "./*.html", "./ecommerce/*.html",
+      "./assets/**/*.js",
+      "./node_modules/flowbite/**/*.js",
+      "./src/**/*.png",
+      "./src/**/*.aviv"
+  ],
+  theme: {
+     extend: {
+      colors: {
+        "color-primary": "#01051e",
+        "color-primary-light": "#020726",
+        "color-primary-dark": "#010417",
+        "color-secondary": "#ff7d3b",
+        "color-gray": "#333",
+        "color-white": "#fff",
+        "color-blob": "#A427DF",
+      }
+     },
+  },
+  plugins: [
+     require('@tailwindcss/forms'),
+     require('preline/plugin'),
+  ]
+ }
+ 
